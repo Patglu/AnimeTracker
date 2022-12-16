dd3204b44b74e55a739325e0ae8d72925366f930
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-12-16 11:36:14 +0200

    Initial Commit

---
+++ AnimeTracker/AnimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerApp.swift
+//  AnimeTracker
+//
+//  Created by Pat on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct AnimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
