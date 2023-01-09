bd91e11e76cf2baec66897bf3408c87d504f4037
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-09 15:37:01 +0200

    URL Sessions Extensions added

---
+++ AnimeTracker/Networking /URL+Extension.swift
@@ -0,0 +1,23 @@
+import Foundation
+extension URLSession{
+    enum CustomError: Error{
+        case invalidUrl
+        case invalidData
+    }
+}
+
+extension URLSession{
+    func request<T: Decodable>(url: URL,
+                               completion: @escaping (T?, Error?) -> Void) {
+        let request = URLRequest(url: url, timeoutInterval: 5)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            var decodedResponse: T? = nil
+            if let data = data {
+                decodedResponse = try? JSONDecoder().decode(T.self, from: data)
+            }
+            completion(decodedResponse, error)
+        }
+        task.resume()
+    }
+}
+
