e9784b17eb5a953b8d4a7a7dc833e81445e3ebba
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-09 15:59:49 +0200

    Model Rework

---
+++ AnimeTracker/Model/BasicAnimeModel.swift
@@ -0,0 +1,455 @@
+
+import Foundation
+
+// MARK: - BasicAnimeModel
+struct BasicAnimeModel: Codable {
+    let pagination: Pagination
+    let data: [Datum]
+    let links: Links
+    let meta: Meta
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let malID: Int
+    let images: [String: AnimeImage]
+    var image: String {
+        
+    }
+    let title: String
+    let type: DatumType
+    enum CodingKeys: String, CodingKey {
+        case malID = "mal_id"
+        case url, images, trailer, approved, titles, title
+        case titleEnglish = "title_english"
+        case titleJapanese = "title_japanese"
+        case titleSynonyms = "title_synonyms"
+        case type, source, episodes, status, airing, aired, duration, rating, score
+        case scoredBy = "scored_by"
+        case rank, popularity, members, favorites, synopsis, background, season, year, broadcast, producers, licensors, studios, genres
+        case explicitGenres = "explicit_genres"
+        case themes, demographics
+    }
+}
+
+// MARK: - Aired
+struct Aired: Codable {
+    let from: Date
+    let to: Date?
+    let prop: Prop
+    let string: String
+}
+
+// MARK: - Prop
+struct Prop: Codable {
+    let from, to: From
+}
+
+// MARK: - From
+struct From: Codable {
+    let day, month, year: Int?
+}
+
+// MARK: - Broadcast
+struct Broadcast: Codable {
+    let day, time: String?
+    let timezone: Timezone?
+    let string: String?
+}
+
+enum Timezone: String, Codable {
+    case asiaTokyo = "Asia/Tokyo"
+}
+
+// MARK: - Demographic
+struct Demographic: Codable {
+    let malID: Int
+    let type: DemographicType
+    let name: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case malID = "mal_id"
+        case type, name, url
+    }
+}
+
+enum DemographicType: String, Codable {
+    case anime = "anime"
+}
+
+// MARK: - Image
+struct AnimeImage: Codable {
+    let imageURL, smallImageURL, largeImageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "image_url"
+        case smallImageURL = "small_image_url"
+        case largeImageURL = "large_image_url"
+    }
+}
+
+enum Rating: String, Codable {
+    case pg13Teens13OrOlder = "PG-13 - Teens 13 or older"
+    case r17ViolenceProfanity = "R - 17+ (violence & profanity)"
+    case rMildNudity = "R+ - Mild Nudity"
+}
+
+enum Season: String, Codable {
+    case fall = "fall"
+    case spring = "spring"
+    case summer = "summer"
+    case winter = "winter"
+}
+
+enum Status: String, Codable {
+    case finishedAiring = "Finished Airing"
+}
+
+// MARK: - Title
+struct Title: Codable {
+    let type: TitleType
+    let title: String
+}
+
+enum TitleType: String, Codable {
+    case english = "English"
+    case french = "French"
+    case german = "German"
+    case japanese = "Japanese"
+    case spanish = "Spanish"
+    case synonym = "Synonym"
+    case typeDefault = "Default"
+}
+
+// MARK: - Trailer
+struct Trailer: Codable {
+    let youtubeID: String?
+    let url, embedURL: String?
+    let images: Images
+
+    enum CodingKeys: String, CodingKey {
+        case youtubeID = "youtube_id"
+        case url
+        case embedURL = "embed_url"
+        case images
+    }
+}
+
+// MARK: - Images
+struct Images: Codable {
+    let imageURL, smallImageURL, mediumImageURL, largeImageURL: String?
+    let maximumImageURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "image_url"
+        case smallImageURL = "small_image_url"
+        case mediumImageURL = "medium_image_url"
+        case largeImageURL = "large_image_url"
+        case maximumImageURL = "maximum_image_url"
+    }
+}
+
+enum DatumType: String, Codable {
+    case movie = "Movie"
+    case ova = "OVA"
+    case tv = "TV"
+}
+
+// MARK: - Links
+struct Links: Codable {
+    let first, last: String
+    let prev: JSONNull?
+    let next: String
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+    let currentPage, from, lastPage: Int
+    let links: [Link]
+    let path: String
+    let perPage, to, total: Int
+
+    enum CodingKeys: String, CodingKey {
+        case currentPage = "current_page"
+        case from
+        case lastPage = "last_page"
+        case links, path
+        case perPage = "per_page"
+        case to, total
+    }
+}
+
+// MARK: - Link
+struct Link: Codable {
+    let url: String?
+    let label: String
+    let active: Bool
+}
+
+// MARK: - Pagination
+struct Pagination: Codable {
+    let lastVisiblePage: Int
+    let hasNextPage: Bool
+    let currentPage: Int
+    let items: Items
+
+    enum CodingKeys: String, CodingKey {
+        case lastVisiblePage = "last_visible_page"
+        case hasNextPage = "has_next_page"
+        case currentPage = "current_page"
+        case items
+    }
+}
+
+// MARK: - Items
+struct Items: Codable {
+    let count, total, perPage: Int
+
+    enum CodingKeys: String, CodingKey {
+        case count, total
+        case perPage = "per_page"
+    }
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
+
+class JSONCodingKey: CodingKey {
+    let key: String
+
+    required init?(intValue: Int) {
+        return nil
+    }
+
+    required init?(stringValue: String) {
+        key = stringValue
+    }
+
+    var intValue: Int? {
+        return nil
+    }
+
+    var stringValue: String {
+        return key
+    }
+}
+
+class JSONAny: Codable {
+
+    let value: Any
+
+    static func decodingError(forCodingPath codingPath: [CodingKey]) -> DecodingError {
+        let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Cannot decode JSONAny")
+        return DecodingError.typeMismatch(JSONAny.self, context)
+    }
+
+    static func encodingError(forValue value: Any, codingPath: [CodingKey]) -> EncodingError {
+        let context = EncodingError.Context(codingPath: codingPath, debugDescription: "Cannot encode JSONAny")
+        return EncodingError.invalidValue(value, context)
+    }
+
+    static func decode(from container: SingleValueDecodingContainer) throws -> Any {
+        if let value = try? container.decode(Bool.self) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self) {
+            return value
+        }
+        if let value = try? container.decode(Double.self) {
+            return value
+        }
+        if let value = try? container.decode(String.self) {
+            return value
+        }
+        if container.decodeNil() {
+            return JSONNull()
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+
+    static func decode(from container: inout UnkeyedDecodingContainer) throws -> Any {
+        if let value = try? container.decode(Bool.self) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self) {
+            return value
+        }
+        if let value = try? container.decode(Double.self) {
+            return value
+        }
+        if let value = try? container.decode(String.self) {
+            return value
+        }
+        if let value = try? container.decodeNil() {
+            if value {
+                return JSONNull()
+            }
+        }
+        if var container = try? container.nestedUnkeyedContainer() {
+            return try decodeArray(from: &container)
+        }
+        if var container = try? container.nestedContainer(keyedBy: JSONCodingKey.self) {
+            return try decodeDictionary(from: &container)
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+
+    static func decode(from container: inout KeyedDecodingContainer<JSONCodingKey>, forKey key: JSONCodingKey) throws -> Any {
+        if let value = try? container.decode(Bool.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(Double.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(String.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decodeNil(forKey: key) {
+            if value {
+                return JSONNull()
+            }
+        }
+        if var container = try? container.nestedUnkeyedContainer(forKey: key) {
+            return try decodeArray(from: &container)
+        }
+        if var container = try? container.nestedContainer(keyedBy: JSONCodingKey.self, forKey: key) {
+            return try decodeDictionary(from: &container)
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+
+    static func decodeArray(from container: inout UnkeyedDecodingContainer) throws -> [Any] {
+        var arr: [Any] = []
+        while !container.isAtEnd {
+            let value = try decode(from: &container)
+            arr.append(value)
+        }
+        return arr
+    }
+
+    static func decodeDictionary(from container: inout KeyedDecodingContainer<JSONCodingKey>) throws -> [String: Any] {
+        var dict = [String: Any]()
+        for key in container.allKeys {
+            let value = try decode(from: &container, forKey: key)
+            dict[key.stringValue] = value
+        }
+        return dict
+    }
+
+    static func encode(to container: inout UnkeyedEncodingContainer, array: [Any]) throws {
+        for value in array {
+            if let value = value as? Bool {
+                try container.encode(value)
+            } else if let value = value as? Int64 {
+                try container.encode(value)
+            } else if let value = value as? Double {
+                try container.encode(value)
+            } else if let value = value as? String {
+                try container.encode(value)
+            } else if value is JSONNull {
+                try container.encodeNil()
+            } else if let value = value as? [Any] {
+                var container = container.nestedUnkeyedContainer()
+                try encode(to: &container, array: value)
+            } else if let value = value as? [String: Any] {
+                var container = container.nestedContainer(keyedBy: JSONCodingKey.self)
+                try encode(to: &container, dictionary: value)
+            } else {
+                throw encodingError(forValue: value, codingPath: container.codingPath)
+            }
+        }
+    }
+
+    static func encode(to container: inout KeyedEncodingContainer<JSONCodingKey>, dictionary: [String: Any]) throws {
+        for (key, value) in dictionary {
+            let key = JSONCodingKey(stringValue: key)!
+            if let value = value as? Bool {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? Int64 {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? Double {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? String {
+                try container.encode(value, forKey: key)
+            } else if value is JSONNull {
+                try container.encodeNil(forKey: key)
+            } else if let value = value as? [Any] {
+                var container = container.nestedUnkeyedContainer(forKey: key)
+                try encode(to: &container, array: value)
+            } else if let value = value as? [String: Any] {
+                var container = container.nestedContainer(keyedBy: JSONCodingKey.self, forKey: key)
+                try encode(to: &container, dictionary: value)
+            } else {
+                throw encodingError(forValue: value, codingPath: container.codingPath)
+            }
+        }
+    }
+
+    static func encode(to container: inout SingleValueEncodingContainer, value: Any) throws {
+        if let value = value as? Bool {
+            try container.encode(value)
+        } else if let value = value as? Int64 {
+            try container.encode(value)
+        } else if let value = value as? Double {
+            try container.encode(value)
+        } else if let value = value as? String {
+            try container.encode(value)
+        } else if value is JSONNull {
+            try container.encodeNil()
+        } else {
+            throw encodingError(forValue: value, codingPath: container.codingPath)
+        }
+    }
+
+    public required init(from decoder: Decoder) throws {
+        if var arrayContainer = try? decoder.unkeyedContainer() {
+            self.value = try JSONAny.decodeArray(from: &arrayContainer)
+        } else if var container = try? decoder.container(keyedBy: JSONCodingKey.self) {
+            self.value = try JSONAny.decodeDictionary(from: &container)
+        } else {
+            let container = try decoder.singleValueContainer()
+            self.value = try JSONAny.decode(from: container)
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        if let arr = self.value as? [Any] {
+            var container = encoder.unkeyedContainer()
+            try JSONAny.encode(to: &container, array: arr)
+        } else if let dict = self.value as? [String: Any] {
+            var container = encoder.container(keyedBy: JSONCodingKey.self)
+            try JSONAny.encode(to: &container, dictionary: dict)
+        } else {
+            var container = encoder.singleValueContainer()
+            try JSONAny.encode(to: &container, value: self.value)
+        }
+    }
+}
--- AnimeTracker/Networking /MyAnimeListManager.swift
+++ AnimeTracker/Networking /MyAnimeListManager.swift
@@ -2,9 +2,12 @@ import Foundation
 import Combine
 
 class AnimeTrackerManager: ObservableObject{
+//    URLSession.shared.request(url: url) { [weak self] (animeData: RecommendedAnime?, error: Error?) in
+//        guard let safeAnimeData = animeData else { return }
+//        DispatchQueue.main.async {
+//            self?.trendingAnime = safeAnimeData
+//        }
+//    }
+//
 
-    init() {
-  
-    }
-    
 }
